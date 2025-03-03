ebccf812281a9529c98cf7fe4a8172e81d25ad45
Markonav <marconavarroioo7@gmail.com>
2025-03-03 19:42:43 -0300

    Create Markonav.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Markonav.py
@@ -0,0 +1,17 @@
+#https://www.python.org/
+
+#Comentario
+
+"""
+Comentario en varias lineas
+
+"""
+#Tipos de variables
+
+int_variable = 43
+float_variable = 2.4
+bool_variable = True
+string_variable = "Cadena"
+CONSTANTE = "No cambia"     #Constante por convención
+
+print("Hello, World")
